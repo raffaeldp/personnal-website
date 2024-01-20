c6a2cae6b6406bb4bc52cb845fb07cb0c1ad0da2
Raffael DiPietro <maxou4170@gmail.com>
2024-01-20 14:10:55 +0100

    feat(sanity): install sanity with studio

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
+  },
+  experimental: {
+    taint: true,
+  },
 }
 
 module.exports = nextConfig
