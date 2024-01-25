0154f6f5d9882467794eb044bf38a7245e3cf592
Raffael <maxou4170@gmail.com>
2024-01-25 23:51:39 +0100

    refactor(project): change techno position

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -26,22 +26,27 @@ export default function Project({ project }: { project: Project }) {
         </div>
       </header>
       <main className="mt-4">
-        <div className="flex flex-col items-center lg:flex-row lg:items-start lg:justify-between lg:px-8">
+        <div className="lg:px-8">
           <div className="flex flex-col gap-4">
             {project.projectLinks ? (
               <ProjectLinks projectLinks={project.projectLinks} />
             ) : null}
             <div className="divider"></div>
-            <article className="container prose rounded-xl pt-4 md:prose-base 2xl:prose-xl max-md:max-w-full md:ml-16">
-              {body ? <CustomPortableText value={body} /> : null}
-            </article>
-          </div>
-          <div className="h-fit w-fit rounded-xl bg-base-100 p-4 shadow-xl">
-            <p>Created with</p>
-            <div className="flex h-fit gap-3 rounded-xl pt-4 md:flex-col">
-              {technologies?.map((technology, index) => (
-                <TechnologyTag name={technology} key={index}></TechnologyTag>
-              ))}
+            <div className="flex flex-col items-center pt-4 lg:flex-row lg:items-start lg:justify-between ">
+              <article className="container prose pt-4 md:prose-base 2xl:prose-xl max-md:max-w-full md:ml-16">
+                {body ? <CustomPortableText value={body} /> : null}
+              </article>
+              <div className="h-fit w-fit rounded-xl bg-base-100 p-4 shadow-xl">
+                <p>Created with</p>
+                <div className="flex h-fit flex-col gap-3 rounded-xl pt-4">
+                  {technologies?.map((technology, index) => (
+                    <TechnologyTag
+                      name={technology}
+                      key={index}
+                    ></TechnologyTag>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
