a7de63ca1da7eb3ba78fe5dc2b631103ab418430
Raffael <maxou4170@gmail.com>
2024-01-26 23:37:13 +0100

    refactor(projectCard): set max technologies to display

--- src/components/project/projectCard.tsx
+++ src/components/project/projectCard.tsx
@@ -5,6 +5,8 @@ export default function ProjectCardComponent({
   title,
   technologies,
 }: Partial<Project>) {
+  const MAX_TECHNOLOGIES = 2
+
   return (
     <div className="card card-compact w-96 rounded-xl bg-base-100 shadow-xl transition-transform duration-300 animate-in slide-in-from-top-4 hover:z-10 hover:scale-105">
       <figure>
@@ -14,9 +16,16 @@ export default function ProjectCardComponent({
       </figure>
       <div className="card-body">
         <div className="flex flex-wrap gap-2">
-          {technologies?.map((technology, index) => (
-            <TechnologyTag key={index} name={technology.name} />
-          ))}
+          {technologies
+            ?.slice(0, MAX_TECHNOLOGIES)
+            .map((technology, index) => (
+              <TechnologyTag key={index} name={technology.name} />
+            ))}
+          {technologies && technologies?.length > MAX_TECHNOLOGIES ? (
+            <TechnologyTag
+              name={`+${technologies.length - MAX_TECHNOLOGIES} ...`}
+            />
+          ) : null}
         </div>
       </div>
     </div>
