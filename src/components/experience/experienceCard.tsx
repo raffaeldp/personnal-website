5cbdb859a8180021c4c9854955ea78806ffde699
Raffael Di Pietro <maxou4170@gmail.com>
2024-10-01 22:31:36 +0200

    feat(experienceCard): learn more button in thin weight

--- src/components/experience/experienceCard.tsx
+++ src/components/experience/experienceCard.tsx
@@ -8,7 +8,7 @@ export default function ExperienceCard({
   experience: Experience
 }) {
   return (
-    <div className="card card-compact max-w-lg flex-row gap-4 rounded-xl bg-base-100 p-4 shadow-xl transition-transform duration-300 animate-in slide-in-from-top-4 hover:z-10 hover:scale-105">
+    <div className="card card-compact max-w-lg flex-row gap-4 rounded-xl bg-base-100 p-4 shadow-xl transition-transform animate-in slide-in-from-top-4 duration-300 hover:z-10 hover:scale-105">
       <div className="flex h-8 w-8 shrink-0 items-center justify-center rounded-full bg-white">
         <HeaderPortableImage
           classesImage="rounded-full"
@@ -28,7 +28,7 @@ export default function ExperienceCard({
         <div className="mt-4 line-clamp-3">
           <PortableText value={experience.body}></PortableText>
         </div>
-        <button className="btn btn-link mt-4 h-fit min-h-fit self-end p-0 no-underline">
+        <button className="btn-link btn mt-4 h-fit min-h-fit self-end p-0 font-thin no-underline">
           Learn more
         </button>
       </div>
